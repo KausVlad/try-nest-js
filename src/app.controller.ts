0f315f3bc76a812fa4a52a1da261147702dddbc7
Vlad Sh <kausvirtual@gmail.com>
2023-08-12 02:34:52 +0300

    crateReport

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { EnumReportType, data } from './data';
+import { v4 as uuid } from 'uuid';
 
 @Controller('report/:typeReport')
 export class AppController {
@@ -24,5 +25,26 @@ export class AppController {
     return data.report.filter(
       (report) => report.type === reportType && report.id === id,
     );
+  }
+
+  @Post()
+  crateReport(
+    @Body() { amount, source }: { source: string; amount: number },
+    @Param('typeReport') typeReport: string,
+  ) {
+    const newReport = {
+      id: uuid(),
+      source,
+      amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type:
+        typeReport === 'income'
+          ? EnumReportType.INCOME
+          : EnumReportType.EXPENSE,
+    };
+
+    data.report.push(newReport);
+    return newReport;
   }
 }
